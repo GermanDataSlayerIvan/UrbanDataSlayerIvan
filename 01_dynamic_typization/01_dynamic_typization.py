f4d95d79385317a4746385d9188e5dc1a787e48e
ivanshulika <ivanshulika96@yandex.ru>
2024-10-16 21:45:28 +0300

    10/09/2024 10/09/2024 - четвёртое задание.

---
+++ 01_dynamic_typization/01_dynamic_typization.py
@@ -0,0 +1,13 @@
+#1st program
+
+name = "Ivan"
+print("Name: " + name)
+
+age = 28
+print("Age: " + str(age))
+
+age += 2
+print("New Age: " + str(age))
+
+is_student = True
+print("Is Student: " + str(is_student))
